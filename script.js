6d6c7d4f40c9abd9aea08d2fa12fdb3785dd6423
RaheelArif <phuawei292@gmail.com>
2019-08-08 18:34:08 +0500

    js lecture two

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert ("hello from another file");
+var fee = 2000 ;
+alert(fee + 0 + 1000 - 50 + 50);
+
+
+
+
+// fee = fee + 0;
+// fee = fee + 1000;
+// alert(fee);
+// fee = fee - 50;
+// alert(fee);
