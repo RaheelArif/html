81a965f0129bac047721b907359b7b694bc3ba87
raheelarif <raheelarif>
2019-08-21 18:13:21 +0500

    8 and 9  chapter

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-var a = 5 ;
-var b = 2 ;
-var c = a % b;
+var num1 =  prompt("enter first number" , 2);
+var num2 = prompt("enter second number" , 2);
+var sum =  Number(num1) + Number(num2);
 
-
-
-
-alert(c);
-
-
-
+alert( "num1 + num2 = "  + sum );
