d0ce25d21c77d74f0f664dbdf1bc5bf22aff98f4
raheelarif <raheelarif>
2019-08-23 18:29:09 +0500

    if else second lecture

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 
-var sum = prompt(" 2 + 2 = ?");
+var study = "matric" ;
+var rishwat = "no" ;
+var sifarish = "yess" ;
 
-if(sum == 4){
-   alert("correct answer")
+if(sifarish == "no"){
+    if(study == "matric"){
+        alert("you can get job")
+    }
+}else if(rishwat == "yess"){
+    alert("you can get job because of rishwat")
 }else{
-   alert("incorrect")
+    alert("you cant")
 }
+
+
+// if(  rishwat == "yess" || sifarish == "yess" && study == "matric" ){
+//     alert("you can get job")
+// }else{
+//     alert("sorry you cant")
+// }
+ 
