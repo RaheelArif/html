57593d253ec5b0cb9516cc9442388d17135f636e
raheelarif <raheelarif>
2019-08-28 17:47:17 +0500

    arrays

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
+var student1 = "ali";
+var student2 = "ali2";
+var student3 = "ali3";
+var student4 = "ali4";
+var student5 = "ali5";
 
-var study = "matric" ;
-var rishwat = "no" ;
-var sifarish = "yess" ;
+var student = ["ali" , "ali2" , student3 , "ali4" , "ali5"]
+student.push("ali6" )
+student.pop()
 
-if(sifarish == "no"){
-    if(study == "matric"){
-        alert("you can get job")
-    }
-}else if(rishwat == "yess"){
-    alert("you can get job because of rishwat")
-}else{
-    alert("you cant")
-}
-
-
-// if(  rishwat == "yess" || sifarish == "yess" && study == "matric" ){
-//     alert("you can get job")
-// }else{
-//     alert("sorry you cant")
-// }
- 
+alert(student[5])
