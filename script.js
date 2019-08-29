6a8d3ab54f262a1ee13ab1964573b01dbd882e22
raheelarif <raheelarif>
2019-08-29 18:36:11 +0500

    17 chapter

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-
-var number = prompt("enter a number for table");
-for(var a = 1; a <= 10; a++){
-    alert(a * number)
-}
-
-
+var students = ["ali1" , "ali2", "ali3" ,"ali4" , "ali5", "ali6", "ali7" , "ali8", "ali9", "ali10"];
+students.splice(4 , 0, "ali4.1", "ali4.2")
+alert(students);
+// var newArray = students.splice(9, 1)
+// alert(newArray )
 
 
-
-
-
-// var student = ["ali" , "ali2" ,"ali3", "ali4" , "ali5"]
-
-
-
-
-
-// for(var a = 0; a <= 4; ++a){
-    
-//     alert(student[a])
-// }
-
-
+// var firstFiveStudents = students.slice(3, 5)
+// students.unshift( "ali-1", "ali0")
+// students.shift();
