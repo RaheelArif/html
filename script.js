c8b6fb09091ad6ae631d0927274d49d9f2c9ab3f
raheelarif <raheelarif>
2019-09-15 10:08:12 +0500

    9-15-2019

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 
- function abc(a , b){
-     var answer = a + b;
-     return answer;
-    }
-   var answer2 = abc(2 , 3);
-    alert("your answer is = " +  answer2);
-//     var sum = abc(2 , 3);
+var number1 = prompt("enter first number");
+var number2 = prompt("enter second number");
+var operation = prompt("enter operation");
 
-// for (let index = 1; index < 10; index++) {
-//     console.log(sum * index)
-    
-// }
-
-
-
-// var date = new Date();
-// date.setDate(20)
-// alert(date);
-
-// var date = new Date();
-// var myDate = date.getTime()
-// alert(myDate);
-// var doomsday = new Date("June 30, 2022");
-// var nextDate = doomsday.getTime()
-// alert(nextDate);
+switch (operation) {
+    case "+":
+        alert(number1 + number2);
+        break;
+    case "-":
+        alert(number1 - number2);
+        break;
+    case "/":
+        alert(number1 / number2);
+        break;
+    case "*":
+        alert(number1 * number2);
+        break;
+    default:
+        alert("please enter a valid operation")
+}
