1cd78fe5c9720df9980e755f33f892c180903a9b
raheelarif <raheelarif>
2019-09-21 11:41:27 +0500

    class practice 9/21/19

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-function abc () {
-document.getElementById("img").src = "pic1.jpg"
 
-}
-function abc2 () {
-    document.getElementById("img").src = "pic2.png"
+function changeColor() {
+
+    var number = Math.random() * 5 + 1;
+    number = Math.floor(number);
+    switch (number) {
+        case 1:
+            document.getElementById("bg").className = "red"
+            break;
+        case 2:
+            document.getElementById("bg").className = "green"
+            break;
+        case 3:
+            document.getElementById("bg").className = "blue"
+            break;
+        case 4:
+            document.getElementById("bg").className = "yellow"
+            break;
+            
+            
+            default:
+            document.getElementById("bg").className = "brown"
+
+            break;
+    }
     
+
+    var result = document.getElementById("text").value;
+
+    document.getElementById("result").innerHTML = result;
+    document.getElementById("text").value = "";
 }
+
+
+
